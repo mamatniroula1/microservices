15149892c373f3afad02b9b8b03cd4243d5ca881
mamat <mamatniroula1@gmail.com>
2020-05-28 04:14:21 -0500

    eureka naming server configured

---
+++ src/test/java/com/microservices/Netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.Netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
