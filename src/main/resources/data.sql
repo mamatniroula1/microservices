78a629927df3f36a940937af1457707e4665ca0a
mamat <mamatniroula1@gmail.com>
2020-05-28 16:00:59 -0500

    exchange service completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','NEP',119,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','NEP',140,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'AUD','NEP',70,0);
